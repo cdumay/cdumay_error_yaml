42765e866f2610bc5c72e92b560c0b211e40b2f1
Cédric Dumay <cedric.dumay@gmail.com>
2025-05-15 08:18:24 +0200

    fix: macro full BTreeMap path

--- src/macros.rs
+++ src/macros.rs
@@ -7,12 +7,12 @@ macro_rules! convert_result {
     };
     ($result:expr, $text:expr) => {
         $result.map_err(|err| {
-            cdumay_error_yaml::YamlErrorConverter::convert_error(&err, Some($text.to_string()), BTreeMap::new())
+            cdumay_error_yaml::YamlErrorConverter::convert_error(&err, Some($text.to_string()), std::collections::BTreeMap::new())
         })
     };
     ($result:expr) => {
         $result.map_err(|err| {
-            cdumay_error_yaml::YamlErrorConverter::convert_error(&err, None, BTreeMap::new())
+            cdumay_error_yaml::YamlErrorConverter::convert_error(&err, None, std::collections::BTreeMap::new())
         })
     };
 }
